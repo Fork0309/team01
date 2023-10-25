a859f5c874a0583f606b2b3c53413971d82ebd04
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-10-25 13:29:24 +0800

    Version2.0

--- database/migrations/2023_10_25_044559_create_world_table.php
+++ database/migrations/2023_10_25_044559_create_world_table.php
@@ -16,6 +16,11 @@ class CreateWorldTable extends Migration
         Schema::create('world', function (Blueprint $table) 
         {
             $table->id()->autoIncrement()->unsigned()->comment('所屬區域編號(主鍵)');
+            $table->string('region')->comment('區域');
+            $table->string('rule')->comment('統治');
+            $table->string('attitude')->comment('對於魔法的態度');
+            $table->string('technology')->comment('科技水平');
+            $table->string('environment')->comment('整體環境');
             $table->timestamps();
         });
     }
