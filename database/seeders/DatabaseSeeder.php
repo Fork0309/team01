9c006433d61de23c5308e646e46d55c6eeec58d9
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-11-08 11:26:25 +0800

    新增sedders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(PlayerTableSeeder::class);
+        $this->call(WorldTableSeeder::class);
     }
 }
---
+++ database/seeders/PlayerTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PlayerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        DB::table('player')->insert([
+            'name' => '艾妮維亞',
+            'profession' => '法師',
+            'world' => 1,
+            'usage rate' => 2.80,
+            'winning rate' => 50.30,
+            'ban rate' => 2.10,
+            'skin of shapes' => 14,
+            'listing date' => Carbon::create(2009, 7, 10),
+        ]);
+    }
+}
