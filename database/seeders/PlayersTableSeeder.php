8f3858117342c9169076ec25f2e8bde2cb855ef1
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-11-08 15:49:01 +0800

    內容更新

--- database/seeders/PlayersTableSeeder.php
+++ database/seeders/PlayersTableSeeder.php
@@ -8,23 +8,139 @@ use Illuminate\Support\Facades\DB;
 
 class PlayersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    private $names = 
+    [
+        '艾妮維亞', '艾希', '布郎姆',
+    ];
+    
+    private $nameIndex = 0;
+
+    private $professions = 
+    [
+        '法師', '射手', '輔助',
+    ];
+
+    private $professionIndex = 0;
+
+    private $worlds = 
+    [
+        1, 1, 1,
+    ];
+
+    private $worldIndex = 0;
+
+    private $usagerates = 
+    [
+        2.80, 13.50, 3.20,
+    ];
+
+    private $usagerateIndex = 0;
+
+    private $winningrates = 
+    [
+        50.30, 50.90, 50.40,
+    ];
+
+    private $winningrateIndex = 0;
+
+    private $banrates = 
+    [
+        2.10, 9.20, 0.60,
+    ];
+
+    private $banrateIndex = 0;
+
+    private $skinofshapess = 
+    [
+        14, 17, 16,
+    ];
+
+    private $skinofshapesIndex = 0;
+
+    private $listingdates;
+
+    public function __construct()
+    {
+        $this->listingdates = 
+        [
+            Carbon::create(2009, 7, 10), Carbon::create(2009, 2, 21), Carbon::create(2014, 5, 12),
+        ];
+
+        $this->listingdateIndex = 0;
+    }
+
+    private $listingdateIndex = 0;
+
+    public function Name()
+    {
+        $name = $this->names[$this->nameIndex];
+        $this->nameIndex = ($this->nameIndex + 1) % count($this->names);
+        return $name;
+    }
+
+    public function Profession()
+    {
+        $profession = $this->professions[$this->professionIndex];
+        $this->professionIndex = ($this->professionIndex + 1) % count($this->professions);
+        return $profession;
+    }
+
+    public function World()
+    {
+        $world = $this->worlds[$this->worldIndex];
+        $this->worldIndex = ($this->worldIndex + 1) % count($this->worlds);
+        return $world;
+    }
+
+    public function Usagerate()
+    {
+        $usagerate = $this->usagerates[$this->usagerateIndex];
+        $this->usagerateIndex = ($this->usagerateIndex + 1) % count($this->usagerates);
+        return $usagerate;
+    }
+
+    public function Winningrate()
+    {
+        $winningrate = $this->winningrates[$this->winningrateIndex];
+        $this->winningrateIndex = ($this->winningrateIndex + 1) % count($this->winningrates);
+        return $winningrate;
+    }
+
+    public function Banrate()
+    {
+        $banrate = $this->banrates[$this->banrateIndex];
+        $this->banrateIndex = ($this->banrateIndex + 1) % count($this->banrates);
+        return $banrate;
+    }
+
+    public function Skinofshapes()
+    {
+        $skinofshapes = $this->skinofshapess[$this->skinofshapesIndex];
+        $this->skinofshapesIndex = ($this->skinofshapesIndex + 1) % count($this->skinofshapess);
+        return $skinofshapes;
+    }
+
+    public function Listingdate()
+    {
+        $listingdate = $this->listingdates[$this->listingdateIndex];
+        $this->listingdateIndex = ($this->listingdateIndex + 1) % count($this->listingdates);
+        return $listingdate;
+    }
 
     public function run()
     {
-        DB::table('player')->insert([
-            'name' => '艾妮維亞',
-            'profession' => '法師',
-            'world' => 1,
-            'usage rate' => 2.80,
-            'winning rate' => 50.30,
-            'ban rate' => 2.10,
-            'skin of shapes' => 14,
-            'listing date' => Carbon::create(2009, 7, 10),
-        ]);
+        for ($i = 0; $i < 3; $i++)
+        {
+            DB::table('player')->insert([
+                'name' => $this->Name(),
+                'profession' => $this->Profession(),
+                'world' => $this->World(),
+                'usage rate' => $this->Usagerate(),
+                'winning rate' => $this->Winningrate(),
+                'ban rate' => $this->Banrate(),
+                'skin of shapes' => $this->Skinofshapes(),
+                'listing date' => $this->Listingdate(),
+            ]);
+        }
     }
 }
--- database/seeders/WorldsTableSeeder.php
+++ database/seeders/WorldsTableSeeder.php
@@ -3,24 +3,91 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class WorldsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    private $regions = 
+    [
+        '弗雷爾卓德', '蒂瑪西亞', '諾克薩斯',
+    ];
+    
+    private $regionIndex = 0;
+
+    private $rules = 
+    [
+        '母系部落', '封建君主制', '崇尚擴張主義的帝國',
+    ];
+
+    private $ruleIndex = 0;
+
+    private $attitudes = 
+    [
+        '崇敬', '抵制', '武器化',
+    ];
+    
+    private $attitudeIndex = 0;
+
+    private $technologys = 
+    [
+        '低', '中', '中',
+    ];
+    
+    private $technologyIndex = 0;
+
+    private $environments = 
+    [
+        '冰霜覆蓋的苔原', '富饒的農村', '貧瘠的平原',
+    ];
+    
+    private $environmentIndex = 0;
+
+    public function Region()
+    {
+        $region = $this->regions[$this->regionIndex];
+        $this->regionIndex = ($this->regionIndex + 1) % count($this->regions);
+        return $region;
+    }
+
+    public function Rule()
+    {
+        $rule = $this->rules[$this->ruleIndex];
+        $this->ruleIndex = ($this->ruleIndex + 1) % count($this->rules);
+        return $rule;
+    }
+
+    public function Attitude()
+    {
+        $attitude = $this->attitudes[$this->attitudeIndex];
+        $this->attitudeIndex = ($this->attitudeIndex + 1) % count($this->attitudes);
+        return $attitude;
+    }
+
+    public function Technology()
+    {
+        $technology = $this->technologys[$this->technologyIndex];
+        $this->technologyIndex = ($this->technologyIndex + 1) % count($this->technologys);
+        return $technology;
+    }
+
+    public function Environment()
+    {
+        $environment = $this->environments[$this->environmentIndex];
+        $this->environmentIndex = ($this->environmentIndex + 1) % count($this->environments);
+        return $environment;
+    }
+
     public function run()
     {
-        DB::table('world')->insert([
-            'region' => '弗雷爾卓德',
-            'rule' => '母系部落',
-            'attitude' => '崇敬',
-            'technology' => '低',
-            'environment' => '冰霜覆蓋的苔原',
-        ]);
+        for ($i = 0; $i < 3; $i++)
+        {
+            DB::table('world')->insert([
+                'region' => $this->Region(),
+                'rule' => $this->Rule(),
+                'attitude' => $this->Attitude(),
+                'technology' => $this->Technology(),
+                'environment' => $this->Environment(),
+            ]);
+        }
     }
 }
