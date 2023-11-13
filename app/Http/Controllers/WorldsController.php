8cdfabc216479676dd778072ab6094216e8b04ad
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-11-13 09:58:59 +0800

    新增Controller

---
+++ app/Http/Controllers/WorldsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\World;
+use Illuminate\Http\Request;
+
+class WorldsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\World  $world
+     * @return \Illuminate\Http\Response
+     */
+    public function show(World $world)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\World  $world
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(World $world)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\World  $world
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, World $world)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\World  $world
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(World $world)
+    {
+        //
+    }
+}
