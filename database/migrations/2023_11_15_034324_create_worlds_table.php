3a0cbc17cf6ee69eca9cae379691a7ac400fc295
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-11-15 11:50:03 +0800

    修改Tables檔名

--- database/migrations/2023_11_15_034324_create_worlds_table.php
+++ database/migrations/2023_11_15_034324_create_worlds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class WorldsTable extends Migration
+class CreateWorldsTable extends Migration
 {
     /**
      * Run the migrations.
