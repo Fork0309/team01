845f43568ac6230755ab3e0a2ab969a77d17af5a
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-11-29 09:08:52 +0800

    重新排列網站格式

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -7,17 +7,37 @@
     <body>
         <h1>列出所有角色</h1>
 
-        @for($i=0; $i<count($players); $i++)
-            角色名稱: {{$players[$i]['name'] }} <br/>
-            職業: {{$players[$i]['profession'] }} <br/>
-            所屬區域: {{$players[$i]['world'] }} <br/>
-            使用率: {{$players[$i]['usage_rate'] }} <br/>
-            勝率: {{$players[$i]['winning_rate'] }} <br/>
-            禁用率: {{$players[$i]['ban_rate'] }} <br/>
-            當前造型數量: {{$players[$i]['skin_of_shapes'] }} <br/>
-            上市日期: {{$players[$i]['listing_date'] }} <br/><br/>
-        @endfor
+        <table>
 
+            <tr>
+                <th>角色編號</th>
+                <th>角色名稱</th>
+                <th>角色職業</th>
+                <th>所屬區域</th>
+                <th>使用率</th>
+                <th>勝率</th>
+                <th>禁用率</th>
+                <th>當前造型數量</th>
+                <th>上市時間</th>
+                <th>操作1</th>
+                <th>操作2</th>
+                <th>操作3</th>
+            </tr>
+
+            @for($i=0; $i<count($players); $i++)
+                <tr>
+                    <td>{{ $players[$i]['id']}}</td>
+                    <td>{{ $players[$i]['name']}}</td>
+                    <td>{{ $players[$i]['profession']}}</td>
+                    <td>{{ $players[$i]['world']}}</td>
+                    <td>{{ $players[$i]['usage_rate']}}</td>
+                    <td>{{ $players[$i]['winning_rate']}}</td>
+                    <td>{{ $players[$i]['ban_rate']}}</td>
+                    <td>{{ $players[$i]['skin_of_shapes']}}</td>
+                    <td>{{ $players[$i]['listing_date']}}</td>
+                </tr>
+            @endfor
+
+        </table>
     </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,16 @@ use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\WorldsController;
 use Illuminate\Support\Facades\Route;
 
+// --------------------------- players -------------------------------
+
+// 顯示所有角色的資料
 Route::get('players', [PlayersController::class, 'index'])->name('players.index');
+// 顯示單筆角色的資料
+Route::get('players/{id}', [PlayersController::class, 'show'])->where('id', '[0-9]+')->name('players.show');
+// 修改表單(角色)
+Route::get('players/{id}/edit', [PlayersController::class, 'edit'])->where('id', '[0-9]+')->name('players.edit');
+
+// --------------------------- worlds --------------------------------
+
+// 顯示所有區域的資料
 Route::get('worlds', [WorldsController::class, 'index'])->name('worlds.index');
