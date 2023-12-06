d68dfe48d66194c6562993e9fda6fa37113d5615
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-12-06 11:23:34 +0800

    修改 players.show, 及 worlds.show 的視圖

--- resources/views/worlds/show.blade.php
+++ resources/views/worlds/show.blade.php
@@ -4,4 +4,11 @@
 
 @section('lol_contents')
 
+區域編號:{{$world -> id}} <br/>
+區域:{{$world -> region}} <br/>
+統治:{{$world -> rule}} <br/>
+對於魔法的態度:{{$world -> attitude}} <br/>
+科技水平:{{$world -> technology}} <br/>
+整體環境:{{$world -> environment}} <br/>
+
 @endsection
