be8d3625c3cbc15d5b74d0865fe158c3347d94df
Fork0309 <d1104422014@gm.lhu.edu.tw>
2023-12-13 10:39:57 +0800

    設計和新增角色的表單

--- resources/views/worlds/edit.blade.php
+++ resources/views/worlds/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('app')
 
-@section('title', '英雄聯盟網站 - 修改區域資料')
+@section('title', '英雄聯盟網站 - 更新區域資料')
 
-@section('lol_theme', '修改區域資料')
+@section('lol_theme', '更新區域資料')
 
 @section('lol_contents')
 
     {!! Form::model($world, ['method'=>'PATCH', 'action'=>['\App\Http\Controllers\WorldsController@update', $world->id]]) !!}
-    @include('worlds.form', ['submitButtonText'=>"更新區域資料"])
+        @include('worlds.form', ['submitButtonText'=>"更新區域資料"])
     {!! Form::close() !!}
 
 @endsection
